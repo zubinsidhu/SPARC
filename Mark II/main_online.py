a320de3dfdedaba6d06d3177392ba0c95757ded2
zubinsidhu <145641619+zubinsidhu@users.noreply.github.com>
2025-05-30 17:08:37 -0700

    Create main_online.py

---
+++ Mark II/main_online.py
@@ -0,0 +1,25 @@
+'''
+Important: **Use headphones**. This script uses the system default audio
+input and output, which often won't include echo cancellation. So to prevent
+the model from interrupting itself it is important that you use headphones. 
+
+Before running this script, ensure the `GOOGLE_API_KEY` environment
+variable is set to the api-key you obtained from Google AI Studio.
+'''
+
+from SPARC.SPARC_Online import SPARC
+import asyncio
+
+async def main():
+    sparc = SPARC()
+    async with asyncio.TaskGroup() as tg:
+        tg.create_task(sparc.stt())
+        input_message = tg.create_task(sparc.input_message())
+        tg.create_task(sparc.send_prompt())
+        tg.create_task(sparc.tts())
+        tg.create_task(sparc.play_audio())
+
+        await input_message
+
+if __name__ == "__main__":
+    asyncio.run(main())
